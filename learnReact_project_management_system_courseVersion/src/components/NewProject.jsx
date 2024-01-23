a097deec51ed12a7cd29bc1c3dfb57772245c89f
trx2 <davecen9@gmail.com>
2024-01-23 17:08:24 -0500

    save

--- learnReact_project_management_system_courseVersion/src/components/NewProject.jsx
+++ learnReact_project_management_system_courseVersion/src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Input from "./Input";
 import Modal from "./Modal";
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const modal = useRef();
 
   const titleRef = useRef();
@@ -44,7 +44,7 @@ export default function NewProject({ onAdd }) {
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950">
+            <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
               Cancel
             </button>
           </li>
