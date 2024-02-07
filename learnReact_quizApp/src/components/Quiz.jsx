f69dda541f274ecdee93c2114b07486949bd4730
trx2 <davecen9@gmail.com>
2024-02-06 22:30:07 -0500

    save

--- learnReact_quizApp/src/components/Quiz.jsx
+++ learnReact_quizApp/src/components/Quiz.jsx
@@ -5,6 +5,9 @@ export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(()=> Math.random() - 0.5);
+
 
   function handleSelectAnswer(selectedAnswer){
     setUserAnswers((prevUserAnswers)=>{
@@ -13,15 +16,17 @@ export default function Quiz() {
   }
 
   return (
+    <div id="quiz">
     <div id="question">
       <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
       <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+        {shuffledAnswers.map((answer) => (
           <li key={answer} className="answer">
             <button onClick={()=>handleSelectAnswer(answer)}>{answer}</button>
           </li>
         ))}
       </ul>
+    </div>
     </div>
   );
 }
